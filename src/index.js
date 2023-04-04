4f8879760c4c02c9c2eafda675b0cef8f45a43b4
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-04-04 14:12:04 +0200

    test fn hide load more btn

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ const searchFormEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
 const loadMoreBtnEl = document.querySelector('.load-more');
 const submitBtnEl = document.querySelector('button[type="submit"]');
-loadMoreBtnEl.classList.add('[is-hidden]');
-
-// console.log(submitBtnEl);
-// submitBtnEl.disabled = true;
+loadMoreBtnEl.classList.add('is-hidden');
 
 const newsApiService = new NewsApiService();
 
@@ -20,35 +17,48 @@ function onFormSubmit(event) {
   event.preventDefault();
   newsApiService.query = event.currentTarget.elements.searchQuery.value.trim();
   if (newsApiService.query === '') {
-    loadMoreBtnEl.classList.add('is-hidden');
+    hideLoadMoreBtn(data);
     galleryEl.innerHTML = '';
-    Notiflix.Notify.warning('Enter your search data', {
+    Notiflix.Notify.info('Enter your search data', {
       position: 'center-center',
       clickToClose: true,
     });
     return;
   }
   newsApiService.resetPage();
-  newsApiService.fetchImages().then(data => {
-    if (data.hits.length === 0) {
-      galleryEl.innerHTML = '';
-      Notiflix.Notify.failure(
-        `Sorry, there are no images matching your search query. Please try again. 🤦‍♂️🤔🤪`,
+  newsApiService
+    .fetchImages()
+    .then(data => {
+      if (data.hits.length === 0) {
+        galleryEl.innerHTML = '';
+        hideLoadMoreBtn(data);
+        Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again. 🤦‍♂️🤔🤪`,
+          { position: 'center-center', clickToClose: true }
+        );
+        return;
+      }
+      clearGalleryContainer();
+      appendImageCard(data.hits);
+      loadMoreBtnEl.classList.remove('is-hidden');
+    })
+    .catch(error => {
+      Notiflix.Notify.warning(
+        `Something went wrong during the request. Check your internet connection`,
         { position: 'center-center', clickToClose: true }
       );
-      return;
-    }
-    clearGalleryContainer();
-    appendImageCard(data.hits);
-    loadMoreBtnEl.classList.remove('is-hidden');
-    console.log(data);
-  });
+    });
 }
 
 function onLoadMore() {
   newsApiService.fetchImages().then(data => {
-    console.log(data);
+    hideLoadMoreBtn(data);
     appendImageCard(data.hits);
+  }).catch(error => {
+    Notiflix.Notify.warning(
+      `Something went wrong during the request. Check your internet connection`,
+      { position: 'center-center', clickToClose: true }
+    );
   });
 }
 
@@ -59,3 +69,30 @@ function appendImageCard(images) {
 function clearGalleryContainer() {
   galleryEl.innerHTML = '';
 }
+
+// function disabledsubmitBtn (){
+//   console.log(submitBtnEl);
+//   submitBtnEl.disabled = true;
+// }
+
+function hideLoadMoreBtn(data) {
+  if (data.hits.length < newsApiService.per_page) {
+    loadMoreBtnEl.classList.add('is-hidden');
+    Notiflix.Notify.info(
+      `We're sorry, but you've reached the end of search results.`,
+      {
+        clickToClose: true,
+      }
+    );
+  }
+}
+
+// console.log(data.hits.length < newsApiService.per_page);
+// console.log(newsApiService.per_page);
+// console.log(data.hits.length);
+// console.log(`data.totalHits:`, data.totalHits);
+// console.log(`per_page:`, newsApiService.per_page);
+// console.log(`Поточна сторінка:`, newsApiService.page);
+// console.log(`Відповідь на запит (data):`, data);
+// console.log(`Результатів пошуку на сторінці:`, newsApiService.per_page);
+// console.log(`Екземпляр класу:`, newsApiService);
--- src/js/fetchImage.js
+++ src/js/fetchImage.js
@@ -1,18 +1,23 @@
 const API_KEY = '?key=34921015-82cb8e104c87b6309f3f6f395';
 const BASE_URL = 'https://pixabay.com/api/';
 const FILTER = '&image_type=photo&orientation=horizontal&safesearch=true';
-const COUNT = 4;
 
 export default class NewsApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
+    this.per_page = 3;
   }
 
   fetchImages() {
-    const url = `${BASE_URL}${API_KEY}&q=${this.searchQuery}${FILTER}&per_page=${COUNT}&page=${this.page}`;
+    const url = `${BASE_URL}${API_KEY}&q=${this.searchQuery}${FILTER}&per_page=${this.per_page}&page=${this.page}`;
     return fetch(url)
-    .then(response => response.json())
+    .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
     .then(data => {
       this.incrementPage();
       return data
