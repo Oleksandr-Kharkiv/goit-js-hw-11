625b49a8d28a21dde5a5287aa8d04e88d237363d
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-04-05 14:45:59 +0200

    backup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import NewsApiService from './js/fetchImage';
 import renderImageCard from './js/renderImageCard';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
 const submitBtnEl = document.querySelector('button[type="submit"]');
@@ -12,7 +11,7 @@ const searchFormEl = document.querySelector('#search-form');
 searchFormEl.addEventListener('submit', onFormSubmit);
 
 const inputEl = document.querySelector('input[name="searchQuery"]');
-inputEl.addEventListener('input', debounce(disabledSabmitBtn, 300))
+inputEl.addEventListener('input', debounce(disabledSabmitBtn, 300));
 
 const loadMoreBtnEl = document.querySelector('.load-more');
 loadMoreBtnEl.addEventListener('click', onLoadMorePhoto);
@@ -21,7 +20,7 @@ loadMoreBtnEl.classList.add('is-hidden');
 const newsApiService = new NewsApiService();
 
 function disabledSabmitBtn() {
-  if(inputEl.value === newsApiService.query){
+  if (inputEl.value === newsApiService.query) {
     submitBtnEl.disabled = true;
   } else {
     submitBtnEl.disabled = false;
@@ -35,72 +34,92 @@ function onFormSubmit(event) {
   if (newsApiService.query === '') {
     loadMoreBtnEl.classList.add('is-hidden');
     galleryEl.innerHTML = '';
-    Notiflix.Notify.info('Enter your search data', {
-      position: 'center-center',
-      clickToClose: true,
-    });
+    Notiflix.Notify.info(
+      'Enter your search data. 🤦‍♂️🤔🤪 CHANGE THE SEARCH PARAMETERS!!!🤪🤔🤦‍♂️',
+      {
+        position: 'center-center',
+        clickToClose: true,
+      }
+    );
     return;
   }
   newsApiService.resetPage();
-  newsApiService.fetchImages().then(data => {
-    onLoadMoreBtn (data);
-    if (data.hits.length === 0) {
-      galleryEl.innerHTML = '';
-      Notiflix.Notify.failure(
-        `Sorry, there are no images matching your search query. Please try again. 🤦‍♂️🤔🤪`,
-        { position: 'center-center', clickToClose: true }
+  newsApiService
+    .fetchImages()
+    .then(data => {
+      onLoadMoreBtn(data);
+      if (data.hits.length === 0) {
+        galleryEl.innerHTML = '';
+        Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again. 🤦‍♂️🤔🤪 CHANGE THE SEARCH PARAMETERS!!!🤪🤔🤦‍♂️`,
+          { position: 'center-center', messageMaxLength: 125, clickToClose: true }
         );
         return;
       }
       clearGalleryContainer();
       appendImageCard(data.hits);
-      if(data.hits.length < newsApiService.per_page) {
-        Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`, {
-          clickToClose: true,
-        });
+      if (data.hits.length < newsApiService.per_page) {
+        Notiflix.Notify.info(
+          `We're sorry, but you've reached the end of search results.`,
+          {
+            clickToClose: true,
+          }
+        );
       }
-    }).catch(error => {
-      Notiflix.Notify.warning(`Something went wrong during the request. Check your internet connection`, { position: 'center-center', clickToClose: true })
     })
-    lightbox.refresh();
-  }
-  
-  function onLoadMorePhoto() {
-    newsApiService.fetchImages().then(data => {
+    .catch(error => {
+      Notiflix.Notify.warning(
+        `Something went wrong during the request. Check your internet connection`,
+        { position: 'center-center', clickToClose: true }
+      );
+    });
+  lightbox.refresh();
+}
+
+function onLoadMorePhoto() {
+  newsApiService
+    .fetchImages()
+    .then(data => {
       onLoadMoreBtn(data);
       appendImageCard(data.hits);
-      if(data.hits.length < newsApiService.per_page) {
-        Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`, {
-          clickToClose: true,
-        });
+      if (data.hits.length < newsApiService.per_page) {
+        Notiflix.Notify.info(
+          `We're sorry, but you've reached the end of search results.`,
+          {
+            clickToClose: true,
+          }
+        );
       }
-    }).catch(error => {
-      Notiflix.Notify.warning(`Something went wrong during the request. Check your internet connection`, { position: 'center-center', clickToClose: true })
     })
-    lightbox.refresh();
-  }
+    .catch(error => {
+      Notiflix.Notify.warning(
+        `Something went wrong during the request. Check your internet connection`,
+        { position: 'center-center', clickToClose: true }
+      );
+    });
+  lightbox.refresh();
+}
 
-  function appendImageCard(images) {
-    galleryEl.insertAdjacentHTML('beforeend', renderImageCard(images));
-    let lightbox = new SimpleLightbox(".gallery-photo", {
-      captionsData: 'alt',
-      captionPosition: 'bottom',
-      captionDelay: 250,
+function appendImageCard(images) {
+  galleryEl.insertAdjacentHTML('beforeend', renderImageCard(images));
+  let lightbox = new SimpleLightbox('.gallery-photo', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
   });
-  }
-  
+}
+
 function clearGalleryContainer() {
   galleryEl.innerHTML = '';
 }
 
-function onLoadMoreBtn (data) {
-  if(data.hits.length >= newsApiService.per_page) {
+function onLoadMoreBtn(data) {
+  if (data.hits.length >= newsApiService.per_page) {
     loadMoreBtnEl.classList.remove('is-hidden');
-  }  else if(data.hits.length === 0) {
+  } else if (data.hits.length === 0) {
     loadMoreBtnEl.classList.add('is-hidden');
-  } else if(data.hits.length < newsApiService.per_page) {
+  } else if (data.hits.length < newsApiService.per_page) {
     loadMoreBtnEl.classList.add('is-hidden');
     submitBtnEl.disabled = false;
   }
 }
-
